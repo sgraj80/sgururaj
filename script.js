99fad8ede2f339c98e6c4598eea9ac15e8baffdc
sgraj80 <sgururaj@pdx.edu>
2023-06-05 22:54:38 -0700

    Addded code for go to top button functionality

--- script.js
+++ script.js
@@ -17,3 +17,22 @@ form.addEventListener("submit", (e) => {
 
   alert(`Form submitted successfully at ${currentTime}`);
 });
+
+let mybutton = document.getElementById("topBtn");
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
